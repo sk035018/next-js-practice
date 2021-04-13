f94ec02ad6560c24cda61137bd45bf1f16f3d625
Shivam Kumar <shivamkumar035018@gmail.com>
2021-04-14 00:17:29 +0530

    Layout added to the app and global css added

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,26 @@
 import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
+      <Image
+        src="/images/profile.jpg"
+        alt="Your Name"
+        height={200}
+        width={200}
+      />
       <h2>
         <Link href="/">
           <a>Home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
